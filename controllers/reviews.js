2a83db3807d3ad1aad73e384e9d00c5ddb0b9c89
Frantisek Petruv <f.petruv@gmail.com>
2021-12-03 00:40:33 -0500

    added start script

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,23 +1,22 @@
-const Review = require('../models/review');
-const hospitaldata2 = require('../models/hospitals')
-const User = require('../models/user')
-const express = require('exPress')
-
+const Review = require("../models/review");
+const hospitaldata2 = require("../models/hospitals");
+const User = require("../models/user");
+const express = require("express");
 
 module.exports.createReview = async(req, res) => {
-    const id = req.params.id
-    const hospital = await hospitaldata2.findById(id)
-    const review = new Review(req.body.Review)
-    hospital.Reviews.push(review)
-    await review.save()
-    await hospital.save()
-    res.redirect(`/show/${hospital._id}`)
-}
+    const id = req.params.id;
+    const hospital = await hospitaldata2.findById(id);
+    const review = new Review(req.body.Review);
+    hospital.Reviews.push(review);
+    await review.save();
+    await hospital.save();
+    res.redirect(`/show/${hospital._id}`);
+};
 
 module.exports.deleteReview = async(req, res) => {
-    const { id, reviewId } = req.params; //the only param is the id 
-    await hospitaldata2.findByIdAndUpdate(id, { $pull: { Reviews: reviewId } })
+    const { id, reviewId } = req.params; //the only param is the id
+    await hospitaldata2.findByIdAndUpdate(id, { $pull: { Reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
-    req.flash('success', "Successfully deleted") //! FLASH MESSAGES ARE NOT WIORKING 
+    req.flash("success", "Successfully deleted"); //! FLASH MESSAGES ARE NOT WIORKING
     res.redirect(`/show/${id}`); // redirect back to home
-}
+};
